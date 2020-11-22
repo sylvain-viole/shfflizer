3d89d263bb0ed204d0d5af983b8cce7177926994
Sylvavio <sylvavio@gmail.com>
2020-11-22 13:01:22 +0100

    functionnal tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Raleway", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
